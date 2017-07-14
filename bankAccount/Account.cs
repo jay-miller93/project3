da194e3a6ef77d111bd7f2931e043511613bd264
Jay <isaacsdaddy42293@gmail.com>
2017-07-14 18:21:49 -0400

    progress commit

---
+++ bankAccount/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankAccount
+{
+    class Account//base class
+    {
+        //fields
+        protected int accountNumber;
+        protected decimal checkingBalance;
+        protected decimal savingsBalance;
+
+        //properties
+        public int AccountNumber { get; }
+        public decimal CheckingBalance { get; }
+        public decimal SavingsBalance { get; }
+
+        //constructors
+        public Account()
+        {
+             
+        }
+        public Account(int accountNumber, decimal checkingBalance, decimal savingsBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.checkingBalance = checkingBalance;
+            this.savingsBalance = savingsBalance;
+        }
+       
+        //method
+        public virtual void Withdrawl()
+    }
+}
---
+++ bankAccount/Checking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankAccount
+{
+    class Checking : Account
+    {
+        protected int accountNumber;
+        protected decimal accountBalance;
+
+        //constructor
+        public Checking()
+        {
+
+        }
+     
+       
+    }
+}
